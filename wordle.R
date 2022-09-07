d78369a8b64adde6fd7ea90896a38233e1592427
Jeep <jeppe.ekstrand.halkjaer@gmail.com>
2022-09-07 15:20:00 +0200

    update with Busan changes

--- wordle.R
+++ wordle.R
@@ -6,20 +6,30 @@ library(stringr)
 # Danish
 d <- read.table("RO2012.opslagsord.med.homnr.og.ordklasse.txt", header = FALSE, sep = ";", quote = "\"")
 words <- tolower(d$V1)
+words[substr(words,2,2) == "."] <- substr(words[substr(words,2,2) == "."], 4, max(nchar(words)))
 # English
 d <- words::words
 words <- tolower(d[,1])
 
-mysubset <- function(d, length = 5, notin = NULL, contains = NULL, known = NULL, pos = NULL){
+# words <- gsub("(.)|( )|([1-9])", "", words)
+
+mysubset <- function(d, length = 5, notin = NULL, contains = NULL, known = NULL, pos = NULL, bestWord = FALSE){
   words <- d[nchar(d) == length]
+  words <- d <- words[!unlist(if(length(words) != 0) lapply(strsplit(words, ""), function(i) any(i %in% c("-", " ", "/", ".") )) else character(0))]
+  if(!is.null(notin) & !is.null(contains)) notin <- paste(setdiff(strsplit(notin, "")[[1]], strsplit(contains, "")[[1]]), collapse = "")
   if(!is.null(notin)){
     notin <- strsplit(notin, "")[[1]]
     for(i in 1:length(notin)){
       words <- words[!str_detect(words, notin[i])]  
     }
   }
-  if(!is.null(contains)){
+  if(!is.null(contains) && !identical(contains, "")){
     contains <- strsplit(contains, "")[[1]]
+    known <- as.numeric(strsplit(known, "")[[1]])
+    pos <- as.numeric(strsplit(pos, "")[[1]])
+    if(length(contains) != length(known) | length(known) != length(pos)){
+      warning("contains, known, and pos must have equal length")
+    }
     for(i in 1:length(contains)){
       if(known[i]){
         words <- words[substr(words, pos[i], pos[i]) == contains[i]]    
@@ -30,29 +40,79 @@ mysubset <- function(d, length = 5, notin = NULL, contains = NULL, known = NULL,
       }
     }
   }
-  words <- words[!unlist(lapply(strsplit(words, ""), function(i) any(i %in% c("-", " ", "/", ".") )))]
-  cat("Number of words: ", length(words), "\n")
-  test <- lapply(strsplit(words, ""), function(i) length(unique(i))) == length
-  cat("Word: ", ifelse(any(test == 1), words[match(1, test)], words[1]), "\n")
-  words
+  cat("Number of words:", length(words), "\n")
+  # test <- if(length(words) != 0) lapply(strsplit(words, ""), function(i) length(unique(i))) == length else F
+  
+  if(bestWord){
+    howmany <- rep(NA, length(words))
+    for(i in 1:length(words)){
+      howmany[i] <- checkword(words[i], words)
+    }
+    most <- which(howmany == max(howmany))
+    test <- if(length(most) != 0) lapply(strsplit(words[most], ""), function(i) length(unique(i))) == length else F
+    
+    # cat("Word:", ifelse(any(test == 1), words[match(1, test)], words[1]), "\n")
+    if(any(test == 1)){
+      cat("Words:", sample(words[most[which(test == 1)]], 1), "\n")
+    }
+    else{
+      cat("Words:", sample(words[most], 1), "\n")
+    }
+    # cat("Word:", ifelse(any(test == 1), words[most[which(test == 1)]], words[most]), "\n")
+  }
+  if(!is.null(known) && !identical(known, "")){
+    if(length(unique(pos[known == 1])) == (length - 1) & length(words) >= 3){
+      subwords <- strsplit(words, "")
+      tmp <- rep(NA, length(words))
+      for(i in 1:length(words)) tmp[i] <- subwords[[i]][!((1:length) %in% pos[known == 1])]
+      dsub <- d[!(d %in% words)]
+      
+      for(i in 1:(length-1)){
+        position <- unique(pos[known == 1])[i]
+        dsub <- dsub[substr(dsub, position, position) != unique(substr(words, position, position))]
+      }
+      
+      howmany <- rep(NA, length(dsub))
+      for(i in 1:length(dsub)){
+        howmany[i] <- checkword(dsub[i], tmp)
+      }
+      most <- which(howmany == max(howmany))
+      test <- if(length(most) != 0) lapply(strsplit(dsub[most], ""), function(i) length(unique(i))) == length else F
+      
+      # cat("Word:", ifelse(any(test == 1), words[match(1, test)], words[1]), "\n")
+      if(any(test == 1)){
+        cat("Words:", sample(dsub[most[which(test == 1)]], 1), "\n")
+      }
+      else{
+        cat("Words:", sample(dsub[most],1), "\n")
+      }
+    } 
+  }
+  correct <- readline(prompt = "Correct? ")
+  if(!as.logical(correct)){
+    notinAdd <- readline(prompt = "notin? ")
+    containsAdd <- readline(prompt = "contains? ")
+    knownAdd <- readline(prompt = "known? ")
+    posAdd <- readline(prompt = "pos? ")
+    notin <- paste(c(notin, notinAdd), collapse = "")
+    contains <- paste(c(contains, containsAdd), collapse = "")
+    known <- paste(c(known, knownAdd), collapse = "")
+    pos <- paste(c(pos, posAdd), collapse = "")
+    mysubset(words, length = length, notin = notin, contains = contains, known = known, pos = pos, bestWord = TRUE)
+  }
+  else{
+    cat("You're welcome!")
+  }
   # cat("Words: ", words[1:5], "\n")
 }
 
 
-word <- mysubset(words, length = 5, notin = "randoljusp", 
-                 contains = "cei",
-                 known = c(1,1,1),
-                 pos = c(4,5,3))
+checkword <- function(word, dictionary){
+  word <- paste0("(", paste0(strsplit(word, "")[[1]], collapse = ")|("), ")")
+  sum(grepl(word, dictionary))
+}
 
-word <- mysubset(words, length = 6, notin = "mn", 
-                 contains = "base",
-                 known = c(1,1,1,1),
-                 pos = c(1,2,4,5))
-
-word <- mysubset(words, length = 7, notin = "abikdch", 
-                 contains = "rosse",
-                 known = c(0,1,0,0,0),
-                 pos = c(2,3,4,7,7))
+mysubset(words, length = 5, bestWord = TRUE)
 
 
 
